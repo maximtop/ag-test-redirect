445ead45f1b4145a42d655b3e2a119a58bb81bfa
Maxim Topchu <maximtop@gmail.com>
2018-11-13 11:54:00 +0300

    convert to express app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/first', (req, res) => {
+    res.redirect('/second');
+});
+
+app.get('/second', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/second.html'));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
